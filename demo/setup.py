f39e4ef6d705dcbc47086e2474820ebc3dea3e7c
Zahra Nouri <vkx7ss@virginia.edu>
2024-10-29 20:34:42 -0400

    Demo 09

---
+++ demo/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='3.3',
+    description='A simple demonstration package',
+    url='http://github.com/ontoligent/m09-demo',
+    author='R.C. Alvarado',
+    author_email='ontoligent@gmail.com',
+    license='MIT',
+    packages=['demo']
+)
